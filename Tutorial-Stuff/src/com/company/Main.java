5e5d67f5ddc814b618260d921ae95d52509463e5
Granzer <abdel.94@live.com>
2020-08-25 13:41:37 -0400

    variables stuff
    just doing variables for easy noob way of saving stuff

--- Tutorial-Stuff/src/com/company/Main.java
+++ Tutorial-Stuff/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) { // void means function won't return a value
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA");
+        int age = 30; // saving a variable and only store whole numbers
+        System.out.println(age); //this will print the value of age
     }
 }

f3e9007f8ab1c1d5f1a41280a1b5dc68e2425574
Granzer <abdel.94@live.com>
2020-08-25 21:32:08 -0400

    Primitive Types
    int str bool... all that stuff

--- Tutorial-Stuff/src/com/company/Main.java
+++ Tutorial-Stuff/src/com/company/Main.java
@@ -6,5 +6,13 @@ public class Main {
         //int myAge = 30;// saving a variable and only store whole numbers
         //int herAge = myAge; // takes the value from the top
         //System.out.println(herAge); //this will print the value of myAge
+        //
+        //TYPES
+        byte age = 30;
+        int viewsCount = 123_456_789; // can separate with _ instead of comma make it more readable
+        long viewsCounter = 3_123_456_789L;// using L as a suffix since Java com reads nums as interger by default
+        float price = 10.99F; // using suffix F to show java its a Float... again by default it reads the double
+        char letter = 'A';// char is one letter, STR is group of letters
+        boolean isEligible = true;
     }
 }

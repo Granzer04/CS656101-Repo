4b9d625311c7f09a324d6fcd6af801f2fbf1c37a
Granzer <abdel.94@live.com>
2020-08-27 23:22:10 -0400

    Arrays pt2
    multi dimension arrays with {} and []

--- Tutorial-Stuff/src/com/company/Main.java
+++ Tutorial-Stuff/src/com/company/Main.java
@@ -16,9 +16,14 @@ public class Main {
         //numbers[0] = 1;//set each index with a number and we know that the array is max at size 5
         //numbers[1] = 2;
         //System.out.println(numbers);// this will print addr in memory of the array
-        int[] numbers = { 2, 3, 5, 1, 4 };
+        //int[] numbers = { 2, 3, 5, 1, 4 };
         //System.out.println(numbers.length);//will show size of array
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));//Arrays is a class that will help printing the stuff in the INT Array... make sure you initialize correct Array toString
+        //Arrays.sort(numbers);
+        //System.out.println(Arrays.toString(numbers));//Arrays is a class that will help printing the stuff in the INT Array... make sure you initialize correct Array toString
+        //MultiDimension ARRAY!!
+        //int[][] numbers = new int[2][3];// 2 rows and 3 columns
+        int [][] numbers = { { 1, 2, 3}, { 4, 5, 6} };// Same thing with Row and Column setup
+        numbers[0][0] = 1;//set 1st row 1st column as 1
+        System.out.println(Arrays.deepToString(numbers));//deepToString is for multi D Array
     }
 }
